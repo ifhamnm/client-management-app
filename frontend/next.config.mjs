56e991e86b32562f5e14c644936fd4ccd23d43a7
Ifham Najeem <86495608+ifhamnm@users.noreply.github.com>
2024-08-20 08:25:40 +0530

    Add files via upload

---
+++ frontend/next.config.mjs
@@ -0,0 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'standalone'
+// }
+
+// module.exports = nextConfig
+
+
+const nextConfig = {
+  output: 'standalone'
+};
+
+export default nextConfig;
