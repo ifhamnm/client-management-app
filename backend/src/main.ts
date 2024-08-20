ad5d4cac09c52a3eba7e66014ceb6893f99902e9
Ifham Najeem <86495608+ifhamnm@users.noreply.github.com>
2024-08-20 08:16:56 +0530

    Add files via upload

---
+++ backend/src/main.ts
@@ -0,0 +1,27 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(4000);
+// }
+// bootstrap();
+
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // Replace with your frontend URL if different
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
+  await app.listen(4000);
+}
+bootstrap();
+
